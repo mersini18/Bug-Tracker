196b26febccd8d2ce21f28af74280df6ce4a6eb1
mersini18 <benjaminmersini@hotmail.co.uk>
2024-12-04 21:09:44 +0000

    Added user registration and login with database functionality

--- bug_tracker/run.py
+++ bug_tracker/run.py
@@ -1,6 +1,13 @@
-from app import create_app
+from app import create_app, db
+
 
 app = create_app()
+
+@app.cli.command('init-db')
+def init_db():
+    """Initialize the database."""
+    db.create_all()
+    print("Database initialized!")
 
 if __name__ == "__main__":
     app.run(debug=True)
